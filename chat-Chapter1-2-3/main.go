e74eeb965291f5c2a27567feb76ea956e534c384
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-07-28 23:11:44 +0900

    1.1.1 テンプレート

--- chat-Chapter1-2-3/main.go
+++ chat-Chapter1-2-3/main.go
@@ -3,22 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		  <html>
-		    <head>
-			  <title>チャット</title>
-			</head>
-			<body>
-			  チャットしましょう！
-			</body>
-		  </html>
-		`))
-	})
+// templは1つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
+// ServerHTTPはHTTPリクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// 一番はじめだけテンプレートを作成(コンパイル)
+	// あとのリクエストに対してはすべてそのコンパイルされたテンプレートを利用
+	t.once.Do(func() { // HTTPリクエストがあって初めて実行するか(lazy initialization)前もって実行するかの議論がある
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil) // 注釈: 本当はエラーハンドリングが必要
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"}) // templateHandlerはhttpのHandlerインターフェースを実装しているのでHandleへ渡せる
 	// WEBサーバーを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
