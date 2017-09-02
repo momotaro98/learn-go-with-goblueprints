5325fb9cb270328903654c4246449cd04c61ac7b
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-09-02 12:57:07 +0900

    2.3 エンドポイントと動的なパス

--- chat-Chapter1-2-3/main.go
+++ chat-Chapter1-2-3/main.go
@@ -37,6 +37,7 @@ func main() {
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始
 	go r.run()
