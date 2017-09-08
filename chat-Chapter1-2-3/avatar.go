560505dfe64c132f5d114f5b705eb679262622bc
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-09-09 00:06:18 +0900

    3.3.1 ユーザーの識別

--- chat-Chapter1-2-3/avatar.go
+++ chat-Chapter1-2-3/avatar.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"crypto/md5"
 	"errors"
-	"fmt"
-	"io"
-	"strings"
 )
 
 // ErrNoAvatarURL is an error which is thrown
@@ -38,11 +34,9 @@ type GravatarAvatar struct{}
 var UseGravatarAvatar GravatarAvatar
 
 func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if email, ok := c.userData["email"]; ok {
-		if emailStr, ok := email.(string); ok {
-			m := md5.New()
-			io.WriteString(m, strings.ToLower(emailStr))
-			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
+	if userid, ok := c.userData["userid"]; ok {
+		if useridStr, ok := userid.(string); ok {
+			return "//www.gravatar.com/avatar/" + useridStr, nil
 		}
 	}
 	return "", ErrNoAvatarURL
