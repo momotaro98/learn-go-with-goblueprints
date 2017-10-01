6e56df054cfae8168a01dad11929b17a35a998cd
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-10-01 12:06:04 +0900

    5.3.1 Twitterを使った認証 5.3.1.1 接続の詳細

---
+++ socialpoll-Chapter5-6/twittervotes/twitter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net"
+)
+
+var conn net.Conn
+
+func dial(netw, addr string) (net.Conn, err) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
+
+var reader io.ReadCloser
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
