7ea39156b867984848fe274ea03bf334019fb72f
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-06-23 18:22:59 +0900

    Refactor twittervotes because the original source code changes .And it seems success for Twitter APIs

--- socialpoll-Chapter5-6/twittervotes/main.go
+++ socialpoll-Chapter5-6/twittervotes/main.go
@@ -1,100 +1,199 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"io"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
-	"sync"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
-	"github.com/bitly/go-nsq"
-
+	"github.com/joeshaw/envdecode"
+	"github.com/matryer/go-oauth/oauth"
+	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
 )
 
-var db *mgo.Session
-
-func dialdb() error {
-	var err error
-	log.Println("MongoDBにダイヤル中: localhost")
-	db, err = mgo.Dial("localhost")
-	return err
-}
-
-func closedb() {
-	db.Close()
-	log.Println("データベース接続が閉じられました")
-}
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+	conn       net.Conn
+)
 
 type poll struct {
 	Options []string
 }
-
-func loadOptions() ([]string, error) {
-	var options []string
-	iter := db.DB("ballots").C("polls").Find(nil).Iter()
-	var p poll
-	for iter.Next(&p) {
-		options = append(options, p.Options...)
-	}
-	iter.Close()
-	return options, iter.Err()
+type tweet struct {
+	Text string
 }
 
-func publishVotes(votes <-chan string) <-chan struct{} {
-	stopchan := make(chan struct{}, 1)
-	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+var reader io.ReadCloser
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+
+func main() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	client := &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				if conn != nil {
+					conn.Close()
+					conn = nil
+				}
+				netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+				if err != nil {
+					return nil, err
+				}
+				conn = netc
+				return netc, nil
+			},
+		},
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+	twitterStopChan := make(chan struct{}, 1)
+	publisherStopChan := make(chan struct{}, 1)
+	stop := false
+	signalChan := make(chan os.Signal, 1)
 	go func() {
+		<-signalChan
+		stop = true
+		log.Println("Stopping...")
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	votes := make(chan string) // chan for votes
+	go func() {
+		pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
 		for vote := range votes {
-			pub.Publish("votes", []byte(vote)) // 投票内容をパブリッシュする
+			pub.Publish("votes", []byte(vote)) // publish vote
 		}
 		log.Println("Publisher: Stopping")
 		pub.Stop()
 		log.Println("Publisher: Stopped")
-		stopchan <- struct{}{}
+		publisherStopChan <- struct{}{}
 	}()
-	return stopchan
-}
-
-func main() {
-	var stoplock sync.Mutex
-	stop := false
-	stopChan := make(chan struct{}, 1)
-	signalChan := make(chan os.Signal, 1)
 	go func() {
-		<-signalChan
-		stoplock.Lock()
-		stop = true
-		stoplock.Unlock()
-		log.Println("Stopping...")
-		stopChan <- struct{}{}
-		closeConn()
+		defer func() {
+			twitterStopChan <- struct{}{}
+		}()
+		for {
+			if stop {
+				log.Println("Twitter: Stopped")
+				return
+			}
+			time.Sleep(2 * time.Second) // calm
+			var options []string
+			db, err := mgo.Dial("localhost")
+			if err != nil {
+				log.Fatalln(err)
+			}
+			iter := db.DB("ballots").C("polls").Find(nil).Iter()
+			var p poll
+			for iter.Next(&p) {
+				options = append(options, p.Options...)
+			}
+			iter.Close()
+			db.Close()
+
+			hashtags := make([]string, len(options))
+			for i := range options {
+				hashtags[i] = "#" + strings.ToLower(options[i])
+			}
+
+			form := url.Values{"track": {strings.Join(hashtags, ",")}}
+			formEnc := form.Encode()
+
+			u, _ := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+			req, err := http.NewRequest("POST", u.String(), strings.NewReader(formEnc))
+			if err != nil {
+				log.Println("creating filter request failed:", err)
+			}
+			req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", u, form))
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Println("Error getting response:", err)
+				continue
+			}
+			if resp.StatusCode != http.StatusOK {
+				// this is a nice way to see what the error actually is:
+				s := bufio.NewScanner(resp.Body)
+				s.Scan()
+				log.Println(s.Text())
+				log.Println(hashtags)
+				log.Println("StatusCode =", resp.StatusCode)
+				continue
+			}
+
+			reader = resp.Body
+			decoder := json.NewDecoder(reader)
+			for {
+				var t tweet
+				if err := decoder.Decode(&t); err == nil {
+					for _, option := range options {
+						if strings.Contains(
+							strings.ToLower(t.Text),
+							strings.ToLower(option),
+						) {
+							log.Println("vote:", option)
+							votes <- option
+						}
+					}
+				} else {
+					break
+				}
+			}
+
+		}
+
 	}()
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	if err := dialdb(); err != nil {
-		log.Fatalln("Failed to connect to dial of MongoDB: ", err)
-	}
-	defer closedb()
-
-	// Start Process
-	votes := make(chan string) // 投票結果のためのチャネル
-	publisherStoppedChan := publishVotes(votes)
-	twitterStoppedChan := startTwitterStream(stopChan, votes)
+	// update by forcing the connection to close
 	go func() {
 		for {
 			time.Sleep(1 * time.Minute)
 			closeConn()
-			stoplock.Lock()
 			if stop {
-				stoplock.Unlock()
 				break
 			}
-			stoplock.Unlock()
 		}
 	}()
-	<-twitterStoppedChan
+
+	<-twitterStopChan // important to avoid panic
 	close(votes)
-	<-publisherStoppedChan
+	<-publisherStopChan
+
 }
