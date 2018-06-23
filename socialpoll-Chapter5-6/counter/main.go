52b805a3bfa9df6311445713987fa22c24c17d63
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-06-24 00:27:44 +0900

    5.4.2 NSQ上のメッセージ受信

--- socialpoll-Chapter5-6/counter/main.go
+++ socialpoll-Chapter5-6/counter/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
+	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
 )
 
@@ -35,4 +37,30 @@ func main() {
 		db.Close()
 	}()
 	pollData := db.DB("ballots").C("polls")
+
+	var counts map[string]int
+	var countsLock sync.Mutex
+
+	log.Println("Connecting to NSQ...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
