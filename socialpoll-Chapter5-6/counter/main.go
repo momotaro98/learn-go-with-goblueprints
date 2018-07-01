3ac6c0a406482dde57e37a2fb005db253ae51170
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-02 01:54:02 +0900

    5.4.3 データベースを最新の状態に保つ

--- socialpoll-Chapter5-6/counter/main.go
+++ socialpoll-Chapter5-6/counter/main.go
@@ -6,10 +6,14 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+const updateDuration = 1 * time.Second
 
 var fatalErr error
 
@@ -62,5 +66,37 @@ func main() {
 	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
 		fatal(err)
 		return
+	}
+
+	log.Println("Waiting for votes on nsq...")
+	ticker := time.NewTicker(updateDuration)
+	for {
+		select {
+		case <-ticker.C:
+			doCount(&countsLock, &counts, pollData)
+		}
+	}
+}
+
+func doCount(countsLock *sync.Mutex, counts *map[string]int, pollData *mgo.Collection) {
+	countsLock.Lock()
+	defer countsLock.Unlock()
+	if len(*counts) == 0 {
+		log.Println("No new votes, skipping database update")
+	}
+	log.Println("Updating database...")
+	log.Println(*counts)
+	ok := true
+	for option, count := range *counts {
+		sel := bson.M{"options": bson.M{"$in": []string{option}}}
+		up := bson.M{"$inc": bson.M{"results." + option: count}}
+		if _, err := pollData.UpdateAll(sel, up); err != nil {
+			log.Println("failed to update:", err)
+			ok = false
+		}
+	}
+	if ok {
+		log.Println("Finished updating database...")
+		*counts = nil // rest counts
 	}
 }
