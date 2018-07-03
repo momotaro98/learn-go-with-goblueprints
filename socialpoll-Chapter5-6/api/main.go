55595a04b22f932ebedacfe6e6c051d781d3e7a9
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-03 23:35:50 +0900

    6.3.4 ドメイン間のリソース共有

--- socialpoll-Chapter5-6/api/main.go
+++ socialpoll-Chapter5-6/api/main.go
@@ -6,6 +6,14 @@ import (
 
 func main() {}
 
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
+
 // withAPIKey wraps HandlerFunc
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
