2b492cd4a2f818a64ac69e3fab278b17bb163c5e
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-04 00:37:53 +0900

    6.4 レスポンスの生成

--- socialpoll-Chapter5-6/api/respond.go
+++ socialpoll-Chapter5-6/api/respond.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+func decodeBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
 func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
 	return json.NewEncoder(w).Encode(v)
 }
@@ -27,4 +32,10 @@ func respondErr(w http.ResponseWriter, r *http.Request,
 			"message": fmt.Sprint(args...),
 		},
 	})
+}
+
+func respondHTTPErr(w http.ResponseWriter, r *http.Request,
+	status int,
+) {
+	respondErr(w, r, status, http.StatusText(status))
 }
