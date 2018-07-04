c415b65b5cc4481d24a4d615e2760ade7e9c94f0
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-05 00:57:11 +0900

    6.6 APIを提供するシンプルなmain関数

--- socialpoll-Chapter5-6/api/main.go
+++ socialpoll-Chapter5-6/api/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"flag"
+	"gopkg.in/mgo.v2"
+	"log"
 	"net/http"
 )
 
-func main() {}
+func main() {
+	var (
+		addr  = flag.String("addr", ":8080", "endpoint address")
+		mongo = flag.String("mongo", "localhost", "mongodb address")
+	)
+	log.Println("Dialing mongo", *mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("failed to connect to mongo:", err)
+	}
+	defer db.Close()
+	log.Println(db)
+	/*
+		s := &Server{
+			db: db,
+		}
+	*/
+	mux := http.NewServeMux()
+	// mux.HandleFunc("/polls/", withCORS(withAPIKey(s.handlePolls)))
+	log.Println(mux)
+	log.Println("Starting web server on", *addr)
+	http.ListenAndServe(":8080", mux)
+	log.Println("Stopping....")
+}
+
+// Server is the API server.
+type Server struct {
+	db *mgo.Session
+}
 
 func withCORS(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
