3983a2c7f7adba181930bcabe7947f812d04d6b0
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-29 02:25:06 +0900

    8.4 バックアップを行うデーモン

---
+++ backup-Chapter8/cmds/backupd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/matryer/filedb"
+	"github.com/momotaro98/learn-go-with-goblueprints/backup-Chapter8"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		interval = flag.Duration("interval", 10*time.Second, "interval between checks")
+		archive  = flag.String("archive", "archive", "path to archive location")
+		dbpath   = flag.String("db", "./db", "path to filedb database")
+	)
+	flag.Parse()
+	m := &backup.Monitor{
+		Destination: *archive,
+		Archiver:    backup.ZIP,
+		Paths:       make(map[string]string),
+	}
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+}
