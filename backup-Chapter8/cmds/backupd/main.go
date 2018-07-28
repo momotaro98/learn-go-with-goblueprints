df9614cbcabf1f3cd6dfc687c60965e9f068eaa0
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-07-29 02:34:41 +0900

    8.4.2 データのキャッシュ

--- backup-Chapter8/cmds/backupd/main.go
+++ backup-Chapter8/cmds/backupd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"log"
 	"time"
@@ -8,6 +10,11 @@ import (
 	"github.com/matryer/filedb"
 	"github.com/momotaro98/learn-go-with-goblueprints/backup-Chapter8"
 )
+
+type path struct {
+	Path string
+	Hash string
+}
 
 func main() {
 	var fatalErr error
@@ -36,6 +43,22 @@ func main() {
 	col, err := db.C("paths")
 	if err != nil {
 		fatalErr = err
+		return
+	}
+	var path path
+	col.ForEach(func(_ int, data []byte) bool {
+		if err := json.Unmarshal(data, &path); err != nil {
+			fatalErr = err
+			return true
+		}
+		m.Paths[path.Path] = path.Hash
+		return false // carry on
+	})
+	if fatalErr != nil {
+		return
+	}
+	if len(m.Paths) < 1 {
+		fatalErr = errors.New("no paths - use backup tool to add at least one")
 		return
 	}
 }
