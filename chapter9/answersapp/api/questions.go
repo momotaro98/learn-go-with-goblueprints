2449c183473a9dd4a9069ae3936b7da47025c6ee
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-08-02 00:01:39 +0900

    9.3 Entities and data access

---
+++ chapter9/answersapp/api/questions.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+)
+
+type Question struct {
+	Key          *datastore.Key `json:"id" datastore:"-"`
+	CTime        time.Time      `json:"created"`
+	Question     string         `json:"question"`
+	User         UserCard       `json:"user"`
+	AnswersCount int            `json:"answers_count"`
+}
+
+// OK checks if a Question have no problem.
+func (q Question) OK() error {
+	if len(q.Question) < 10 {
+		return errors.New("question is too short")
+	}
+	return nil
+}
+
+// Create creates a new Question into Datastore
+func (q *Question) Create(ctx context.Context) error {
+	log.Debugf(ctx, "Saving question: %s", q.Question)
+	if q.Key == nil {
+		q.Key = datastore.NewIncompleteKey(ctx, "Question", nil)
+	}
+	user, err := UserFromAEUser(ctx)
+	if err != nil {
+		return nil
+	}
+	q.User = user.Card()
+	q.CTime = time.Now()
+	q.Key, err = datastore.Put(ctx, q.Key, q)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update updates a Question on Datastore.
+func (q *Question) Update(ctx context.Context) error {
+	if q.Key == nil {
+		q.Key = datastore.NewIncompleteKey(ctx, "Question", nil)
+	}
+	var err error
+	q.Key, err = datastore.Put(ctx, q.Key, q)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetQuestion retrieves a Question from Datastore with a key.
+func GetQuestion(ctx context.Context, key *datastore.Key) (*Question, error) {
+	var q Question
+	err := datastore.Get(ctx, key, &q)
+	if err != nil {
+		return nil, err
+	}
+	q.Key = key
+	return &q, nil
+}
