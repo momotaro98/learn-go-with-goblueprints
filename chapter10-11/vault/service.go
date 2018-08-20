05606c5c7bb02b6f7092a0594e21fb8a57c03e5f
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-08-20 20:50:36 +0900

    10.3.0 Building the service

---
+++ chapter10-11/vault/service.go
@@ -0,0 +1,11 @@
+package vault
+
+import "golang.org/x/net/context"
+
+// Service provides password hashing capabilities.
+type Service interface {
+	Hash(ctx context.Context, password string) (string, error)
+	Validate(ctx context.Context, password, hash string) (bool, error)
+}
+
+type vaultService struct {}
