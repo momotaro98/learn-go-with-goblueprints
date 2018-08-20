7494c53d08812ffef40d94c9bae0340786500c01
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-08-20 20:52:49 +0900

    10.3.1 Starting with tests

---
+++ chapter10-11/vault/service_test.go
@@ -0,0 +1,29 @@
+package vault
+
+import (
+	"testing"
+	"golang.org/x/net/context"
+)
+
+func TestHasherService(t *testing.T) {
+	srv := NewService()
+	ctx := context.Background()
+	h, err := srv.Hash(ctx, "password")
+	if err != nil {
+		t.Errorf("Hash: %s", err)
+	}
+	ok, err := srv.Validate(ctx, "password", h)
+	if err != nil {
+		t.Errorf("Valid: %s", err)
+	}
+	if !ok {
+		t.Errorf("Valid: %s", err)
+	}
+	ok, err = srv.Validate(ctx, "wrong password", h)
+	if err != nil {
+		t.Errorf("Valid: %s", err)
+	}
+	if ok {
+		t.Error("expected false from Valid")
+	}
+}
