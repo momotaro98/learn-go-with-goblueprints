b876f5855d9b92d7e9f057033daeb2a1486db3e7
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-08-20 22:03:53 +0900

    10.3.2 Constructors in Go 10.3.3 Hashing and validating passwords with bcrypt

--- chapter10-11/vault/service.go
+++ chapter10-11/vault/service.go
@@ -1,6 +1,9 @@
 package vault
 
-import "golang.org/x/net/context"
+import (
+	"golang.org/x/net/context"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // Service provides password hashing capabilities.
 type Service interface {
@@ -8,4 +11,25 @@ type Service interface {
 	Validate(ctx context.Context, password, hash string) (bool, error)
 }
 
+// NewService makes a new Service.
+func NewService() Service {
+	return vaultService{}
+}
+
 type vaultService struct {}
+
+func (vaultService) Hash(ctx context.Context, password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (vaultService) Validate(ctx context.Context, password, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
