16a259100ed2fc3d129d794a8aea6798a42c00b8
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-08-21 23:34:11 +0900

    10.4.1 Making endpoints for service methods

--- chapter10-11/vault/service.go
+++ chapter10-11/vault/service.go
@@ -1,6 +1,7 @@
 package vault
 
 import (
+	"github.com/go-kit/kit/endpoint"
 	"golang.org/x/net/context"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
@@ -72,4 +73,32 @@ func decodeValidateRequest(ctx context.Context, r *http.Request) (interface{}, e
 
 func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
 	return json.NewEncoder(w).Encode(response)
+}
+
+func MakeHashEndpoint(srv Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(hashRequest)
+		v, err := srv.Hash(ctx, req.Password)
+		if err != nil {
+			// An Error returned from an endpoint is intended to indicate a transport error,
+			// and perhaps Go kit will be configured to retry the call a few times by some
+			// middleware. If our service methods return an error, it is considered a business
+			// logic error and will probably always return the same error for the same input,
+			// so it's not worth retrying. This is why we wrap the error into the response
+			// and return it to the client so that they can deal with it.
+			return hashResponse{v, err.Error()}, nil
+		}
+		return hashResponse{v, ""}, nil
+	}
+}
+
+func MakeValidateEndpoint(srv Service) endpoint.Endpoint {
+	return func (ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(validateRequest)
+		v, err := srv.Validate(ctx, req.Password, req.Hash)
+		if err != nil {
+			return validateResponse{false, err.Error()}, nil
+		}
+		return validateResponse{v, ""}, nil
+	}
 }
