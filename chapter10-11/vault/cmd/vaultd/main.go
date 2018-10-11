2ae8918ebc10e13d4f2368e6a008748581747841
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-10-11 21:27:50 +0900

    10.6.0 Create a server command

---
+++ chapter10-11/vault/cmd/vaultd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"github.com/momotaro98/learn-go-with-goblueprints/chapter10-11/vault"
+	"github.com/momotaro98/learn-go-with-goblueprints/chapter10-11/vault/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var (
+		httpAddr = flag.String("http", ":8080",
+			"http listen address")
+		gRPCAddr = flag.String("grpc", ":8081",
+			"gRPC listen address")
+	)
+
+	flag.Parse()
+	ctx := context.Background()
+	srv := vault.NewService()
+	errChan := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	hashEndpoint := vault.MakeHashEndpoint(srv)
+	validateEndpoint := vault.MakeValidateEndpoint(srv)
+	endpoints := vault.Endpoints{
+		HashEndpoint:     hashEndpoint,
+		ValidateEndpoint: validateEndpoint,
+	}
+
+	// HTTP transport
+	go func() {
+		log.Println("http:", *httpAddr)
+		handler := vault.NewHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	// gRPC transport
+	go func() {
+		listener, err := net.Listen("tcp", *gRPCAddr)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		log.Println("grpc:", *gRPCAddr)
+		handler := vault.NewGRPCServer(ctx, endpoints)
+		gRPCServer := grpc.NewServer()
+		pb.RegisterVaultServer(gRPCServer, handler)
+		errChan <- gRPCServer.Serve(listener)
+	}()
+
+	log.Fatalln(<-errChan)
+}
